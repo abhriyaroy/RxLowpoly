4f58e018ff315fa45b98ca9762b7134837fcfdb1
Abhriya Roy <roy.abhriya@gmail.com>
2019-06-02 06:28:01 +0530

    Rename library so file

---
+++ lowpolyrxjava/src/main/java/com/zebrostudio/lowpolyrxjava/LowPolyRx.kt
@@ -0,0 +1,90 @@
+package com.zebrostudio.lowpolyrxjava
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.Log
+
+class LowPolyRx private constructor() {
+
+  fun lowPoly(input: Bitmap, alphaOrPointCount: Float, fill: Boolean): Bitmap {
+    return generate(input, 50, alphaOrPointCount, true, fill)
+  }
+
+  fun generate(
+    input: Bitmap,
+    threshold: Int,
+    alphaOrPointCount: Float,
+    lowPoly: Boolean,
+    fill: Boolean
+  ): Bitmap {
+    val width = input.width
+    val height = input.height
+
+    val newImage = Bitmap.createBitmap(input.width, input.height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(newImage)
+    val paint = Paint()
+    paint.isAntiAlias = false
+    paint.style = if (fill) Paint.Style.FILL else Paint.Style.STROKE
+    var x1: Int
+    var x2: Int
+    var x3: Int
+    var y1: Int
+    var y2: Int
+    var y3: Int
+    val pixels = IntArray(width * height)
+    input.getPixels(pixels, 0, width, 0, 0, width, height)
+    if (!lowPoly) {
+      Log.w(javaClass.name, "when lowPoly = false, ignore fill boolean.")
+    }
+    val triangles = getTriangles(pixels, width, height, threshold, alphaOrPointCount, lowPoly)
+    if (lowPoly) {
+      val path = Path()
+      var i = 0
+      while (i + 5 < triangles.size) {
+        x1 = triangles[i]
+        y1 = triangles[i + 1]
+        x2 = triangles[i + 2]
+        y2 = triangles[i + 3]
+        x3 = triangles[i + 4]
+        y3 = triangles[i + 5]
+
+        val color = input.getPixel((x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3)
+        path.rewind()
+        path.moveTo(x1.toFloat(), y1.toFloat())
+        path.lineTo(x2.toFloat(), y2.toFloat())
+        path.lineTo(x3.toFloat(), y3.toFloat())
+        path.close()
+        paint.color = color
+        canvas.drawPath(path, paint)
+        i = i + 6
+      }
+    } else {
+      var i = 0
+      val n = triangles.size
+      while (i + 1 < n) {
+        x1 = triangles[i]
+        y1 = triangles[i + 1]
+        val color = input.getPixel(x1, y1)
+        paint.color = color
+        canvas.drawCircle(x1.toFloat(), y1.toFloat(), 1f, paint)
+        i += 2
+      }
+    }
+    return newImage
+  }
+
+  init {
+    System.loadLibrary("lowpoly-lib")
+  }
+
+  private external fun getTriangles(
+    pixels: IntArray,
+    width: Int,
+    height: Int,
+    threshold: Int,
+    alpha_count: Float,
+    lowPoly: Boolean
+  ): IntArray
+}
