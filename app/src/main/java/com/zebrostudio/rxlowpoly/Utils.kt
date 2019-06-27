2a31df5c9a65e573c4f782eb4eb8942b6b2bc462
Abhriya Roy <roy.abhriya@gmail.com>
2019-06-27 20:12:08 +0530

    Add synchronous option to sample app

--- app/src/main/java/com/zebrostudio/rxlowpoly/Utils.kt
+++ app/src/main/java/com/zebrostudio/rxlowpoly/Utils.kt
@@ -1,6 +1,20 @@
 package com.zebrostudio.rxlowpoly
 
 import android.content.Context
+import android.view.View
+import android.view.View.*
 import androidx.annotation.ColorRes
 
 fun Context.colorRes(@ColorRes id: Int) = resources.getColor(id)
+
+fun View.visible() {
+  visibility = VISIBLE
+}
+
+fun View.gone() {
+  visibility = GONE
+}
+
+fun View.invisible() {
+  visibility = INVISIBLE
+}
--- app/src/main/java/com/zebrostudio/rxlowpoly/fragments/BitmapAsyncFragment.kt
+++ app/src/main/java/com/zebrostudio/rxlowpoly/fragments/BitmapAsyncFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.zebrostudio.rxlowpoly.R
+import kotlinx.android.synthetic.main.fragment_bitmap_async.*
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
 class BitmapAsyncFragment : Fragment() {
 
@@ -13,8 +16,21 @@ class BitmapAsyncFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_bitmap_async, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    toolbar.title = "Lowpoly from Bitmap"
+    imageView.setImageDrawable(resources.getDrawable(R.drawable.captain))
+    lowpolyButton.setOnClickListener {
+      convertImageToLowpoly()
+    }
+  }
+
+  private fun convertImageToLowpoly(){
+
   }
 
 }
