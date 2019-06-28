7e4bdf4ef0d0567581ef0a486d753c0ab02b8b00
Abhriya Roy <roy.abhriya@gmail.com>
2019-06-28 15:46:57 +0530

    Make bitmap sync fragment functional

--- app/src/main/java/com/zebrostudio/rxlowpoly/examplefragments/BitmapSyncFragment.kt
+++ app/src/main/java/com/zebrostudio/rxlowpoly/examplefragments/BitmapSyncFragment.kt
@@ -1,13 +1,103 @@
 package com.zebrostudio.rxlowpoly.examplefragments
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.view.View
 import com.zebrostudio.rxlowpoly.R
+import com.zebrostudio.rxlowpoly.RxLowpoly
+import com.zebrostudio.rxlowpoly.showToast
 import com.zebrostudio.rxlowpoly.stringRes
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.fragment_example.view.*
+import java.io.File
+
+private const val SAVE_FILE_NAME = "BitmapSyncExample"
 
 class BitmapSyncFragment : BaseFragment() {
+
   override fun configureView(view: View) {
-    view.toolbarFragment.title =context!!.stringRes(R.string.bitmap_sync_example_fragment_name)
+    view.toolbarFragment.title = context!!.stringRes(R.string.bitmap_sync_example_fragment_name)
     view.imageView.setImageDrawable(resources.getDrawable(R.drawable.captain))
+    view.lowpolyButton.setOnClickListener {
+      if (permissionChecker.isStoragePermissionAvailable(context!!)) {
+        getLowpolyImage(view)
+      } else {
+        requestPermission()
+      }
+    }
   }
+
+  private fun getLowpolyImage(view: View) {
+    disposable = if (shouldSaveToFile) {
+      getLowpolyInFile()
+    } else {
+      getLowpolyInUri()
+    }.observeOn(AndroidSchedulers.mainThread())
+      .doOnSubscribe {
+        showMaterialDialog()
+      }.subscribe({
+        view.imageView.setImageBitmap(it)
+        materialDialog.dismiss()
+        context!!.showToast(SUCCESS_TOAST_MESSAGE)
+      }, {
+        println(it.message)
+        materialDialog.dismiss()
+        context!!.showToast(ERROR_TOAST_MESSAGE + "${it.message}")
+      })
+  }
+
+  private fun getLowpolyInFile(): Single<Bitmap> {
+    return storageHelper.getFileToSaveImage(SAVE_FILE_NAME)
+      .flatMap {
+        convertBitmapToLowpolySyncWithFileOutput(
+          bitmapHelper.drawableToBitmap(
+            resources.getDrawable(
+              R.drawable.captain
+            )
+          ), it
+        )
+      }
+  }
+
+  private fun getLowpolyInUri(): Single<Bitmap> {
+    return storageHelper.getUriToSaveImage(SAVE_FILE_NAME)
+      .flatMap {
+        convertBitmapToLowpolySyncWithUriOutput(
+          bitmapHelper.drawableToBitmap(
+            resources.getDrawable(
+              R.drawable.captain
+            )
+          ), it
+        )
+      }
+  }
+
+  private fun convertBitmapToLowpolySyncWithFileOutput(
+    bitmap: Bitmap,
+    file: File
+  ): Single<Bitmap> {
+    return Single.create {
+      val lowpolyBitmap = RxLowpoly.with(activity!!.applicationContext)
+        .input(bitmap)
+        .overrideScaling(downScalingFactor, maximumWidth)
+        .quality(quality)
+        .output(file)
+        .generate()
+      it.onSuccess(lowpolyBitmap)
+    }
+  }
+
+  private fun convertBitmapToLowpolySyncWithUriOutput(bitmap: Bitmap, uri: Uri): Single<Bitmap> {
+    return Single.create {
+      val lowpolyBitmap = RxLowpoly.with(activity!!.applicationContext)
+        .input(bitmap)
+        .overrideScaling(downScalingFactor, maximumWidth)
+        .quality(quality)
+        .output(uri)
+        .generate()
+      it.onSuccess(lowpolyBitmap)
+    }
+  }
+
 }
