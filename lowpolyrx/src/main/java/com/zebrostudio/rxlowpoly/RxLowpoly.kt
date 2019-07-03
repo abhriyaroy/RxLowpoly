430a0011c9f788a993e5e31494f27d770d0f8376
Abhriya Roy <roy.abhriya@gmail.com>
2019-07-03 10:14:30 +0530

    Remove wrong context exception

--- lowpolyrx/src/main/java/com/zebrostudio/rxlowpoly/RxLowpoly.kt
+++ lowpolyrx/src/main/java/com/zebrostudio/rxlowpoly/RxLowpoly.kt
@@ -1,27 +1,20 @@
 package com.zebrostudio.rxlowpoly
 
-import android.app.Application
 import android.content.Context
 import com.zebrostudio.rxlowpoly.internal.builder.RxLowpolyBuilder
-import com.zebrostudio.rxlowpoly.internal.exceptions.WrongContextException
 
 object RxLowpoly {
 
   /**
    * Entry point of the RxLowpolyBuilder.
-   * Application context is required to run outside of activity scope.
+   * Application context is used to generate Lowpoly independent of the activity scope.
    *
-   *  @param applicationContext context.
+   *  @param context context.
    *  @return [RxLowpolyBuilder].
    */
   @JvmStatic
-  @Throws(WrongContextException::class)
-  fun with(applicationContext: Context): RxLowpolyBuilder {
-    if (applicationContext is Application) {
-      return RxLowpolyBuilder().init(applicationContext)
-    } else {
-      throw WrongContextException()
-    }
+  fun with(context: Context): RxLowpolyBuilder {
+    return RxLowpolyBuilder().init(context.applicationContext)
   }
 
 }
--- lowpolyrx/src/main/java/com/zebrostudio/rxlowpoly/internal/exceptions/WrongContextException.kt
+++
@@ -1,5 +0,0 @@
-package com.zebrostudio.rxlowpoly.internal.exceptions
-
-class WrongContextException(
-  override val message: String = "Context is not application context"
-) : Exception()
